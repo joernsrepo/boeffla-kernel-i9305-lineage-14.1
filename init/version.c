9d92110eab264fa1e837ef68a57cd6d4c88f67e8
rodman01 <email4joern@arcor.de>
2017-04-14 20:44:17 +0200

    change kernel name to @stratonz
    Change-Id: I388825c6dfd3c6b52b04590836ca1a43c0305d79

--- init/version.c
+++ init/version.c
@@ -44,5 +44,5 @@ const char linux_banner[] =
 
 const char linux_proc_banner[] =
 	"%s version %s"
-	" (" LINUX_COMPILE_BY "@" LINUX_COMPILE_HOST ")"
+	" (boeffla_stratonz@rodmans-pc)"
 	" (" LINUX_COMPILER ") %s\n";
