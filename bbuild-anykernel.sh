947547c78179c9f9bb6f8745042a334707c7c2f0
stratonz <fabiotonzar@yahoo.it>
2017-05-18 09:56:17 +0200

    Correction of "int i" not previously declared (reverted from commit 6adba3e7361d890fa8715e6dc02e82b1dcd250b4)

--- bbuild-anykernel.sh
+++ bbuild-anykernel.sh
@@ -10,9 +10,9 @@
 # Parameters to be configured manually
 #######################################
 
-BOEFFLA_VERSION="6.1-boeffla-stratonz-beta6-google-4.8-LOS14.1-i9305"
+BOEFFLA_VERSION="6.1-boeffla-stratonz-beta8-UBERTC-6.0-LOS14.1-i9305"
 
-TOOLCHAIN="/home/fabio/arm-eabi-4.8/bin/arm-eabi-"
+TOOLCHAIN="/home/fabio/UBERTC-arm-eabi-6.0/bin/arm-eabi-"
 ARCHITECTURE=arm
 COMPILER_FLAGS_KERNEL="-mtune=cortex-a9 -fno-diagnostics-show-caret"
 COMPILER_FLAGS_MODULE="-mtune=cortex-a9 -fno-diagnostics-show-caret"
@@ -24,7 +24,7 @@ DTBTOOL_CMD=""
 MODULES_IN_SYSTEM="y"
 OUTPUT_FOLDER=""
 
-DEFCONFIG="beta6_i9305_defconfig"
+DEFCONFIG="beta8_i9305_defconfig"
 DEFCONFIG_VARIANT=""
 
 KERNEL_NAME="Boeffla-Kernel"
